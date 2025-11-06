45fc66ce1f76bee5939b33715d8eb331ac0de6d2
itsFr4nc0 <alexius9753@gmail.com>
2025-11-06 11:04:10 -0500

    Chatbot funcional y menu del carrito

--- src/components/ChatbotButton.tsx
+++ src/components/ChatbotButton.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { ChatWindow } from "./ChatWindow";
 
 export const ChatbotButton: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="chatbot-btn">
-      <img src="/icons/chatbot.svg" alt="Chatbot" />
-    </div>
+    <>
+      <div className="chatbot-btn" onClick={() => setOpen(!open)}>
+        <img src="/icons/chatbot.svg" alt="Chatbot" />
+      </div>
+
+      {open && <ChatWindow onClose={() => setOpen(false)} />}
+    </>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/styles/global.css";
 import { Link } from "react-router-dom";
+import { CartPanel } from "./CartPanel";
 
 export const Header: React.FC = () => {
+  const [openCart, setOpenCart] = useState(false);
+
   return (
-    <header className="header">
-      <h1 className="header-title">Mi Tienda</h1>
-      <div className="header-icons">
-        <img src="/icons/cart.svg" alt="Carrito" />
-        <Link to="/login">
-          <img src="/icons/user.svg" alt="Usuario" />
-        </Link>
-      </div>
-    </header>
+    <>
+      <header className="header">
+        <h1 className="header-title">Mi Tienda</h1>
+        <div className="header-icons">
+          <img
+            src="/icons/cart.svg"
+            alt="Carrito"
+            onClick={() => setOpenCart(!openCart)}
+          />
+          <Link to="/login">
+            <img src="/icons/user.svg" alt="Usuario" />
+          </Link>
+        </div>
+      </header>
+
+      <CartPanel open={openCart} onClose={() => setOpenCart(false)} />
+    </>
   );
 };
